8824f549a916ff8dd290f7c08c25e3dc2b7aafb4
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-11-21 19:49:07 -0300

    Validando um CPF

---
+++ Cpf.py
@@ -0,0 +1,21 @@
+class Cpf:
+    def __init__(self, documento):
+        documento = str(documento)
+        if self.cpf_eh_valido(documento):
+            self.cpf = documento
+        else:
+            raise ValueError("CPF inválido")
+
+    def cpf_eh_valido(self, documento):
+        return len(documento) == 11
+
+    def format_cpf(self):
+        fatia_um = self.cpf[:3]
+        fatia_dois = self.cpf[3:6]
+        fatia_tres = self.cpf[6:9]
+        fatia_quatro = self.cpf[9:]
+
+        return f"{fatia_um}.{fatia_dois}.{fatia_tres}-{fatia_quatro}"
+
+    def __str__(self):
+        return self.format_cpf()
